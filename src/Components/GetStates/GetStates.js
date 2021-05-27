5d1c276fc84825d55a4a6cd121b5c13a72f0423b
Suvansh Shukla <suvanshshukla@gmail.com>
2021-05-27 11:20:42 +0530

    removed material ui inbuilt styling and am using css modules

--- src/Components/GetStates/GetStates.js
+++ src/Components/GetStates/GetStates.js
@@ -1,47 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import classes from './GetStates.module.css';
 import axios from "axios";
 import { TextField, Button } from "@material-ui/core";
 
 //you need to install dotenv using npm then import and configure like below
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.REACT_APP_STATES_API); //the REACT_APP prefix is necessary for naming any env variable
+//console.log(process.env.REACT_APP_STATES_API); //the REACT_APP prefix is necessary for naming any env variable
 //also remember whenever you add a new environment variable you need to restart npm start
 
 
-const a = makeStyles((theme) => ({
-  wrapper: {
-    display: "flex",
-    flexDirection: "column",
-    // justifyContent: "center",
-    alignItems: "center",
-    border: "solid 2px silver",
-    minHeight: "100vh",
-  },
-}));
-
 function GetState() {
-  const classes = a();
   const [info, setInfo] = useState();
   const [name, setName] = useState("");
   const [districts, setDistricts] = useState();
-  const [foundState, setFoundState] = useState(true); //the initial value of foundState is true 
+  const [foundState, setFoundState] = useState(true); //the initial value of foundState is true
 
   //we use this to get state names the first time it the component mounts
   useEffect(() => {
     let incomingData;
-    axios
-      .get(process.env.REACT_APP_STATES_API)
-      .then((res) => {
-        incomingData = res.data.states;
-        if (incomingData != null) {
-          setInfo(incomingData);
-          console.log(incomingData);
-        } else {
-          console.log("incoming Data is null");
-        }
-      });
+    axios.get(process.env.REACT_APP_STATES_API).then((res) => {
+      incomingData = res.data.states;
+      if (incomingData != null) {
+        setInfo(incomingData);
+        console.log(incomingData);
+      } else {
+        console.log("incoming Data is null");
+      }
+    });
   }, []);
 
   /* const getInfo = (a) => {
@@ -68,7 +54,7 @@ function GetState() {
       if (element.state_name === name) {
         console.log(name);
         //here if the state is found then the state will be set to true
-        setFoundState(true)
+        setFoundState(true);
         let district_id = element.state_id;
         axios
           .get(
@@ -78,9 +64,9 @@ function GetState() {
           .then((res) => {
             a = res.data.districts;
             setDistricts(a);
-            console.log(districts);
+            // console.log(districts);
           });
-      } 
+      }
     });
     // if(foundState){setFoundState(true);} else{setFoundState(false)}
     /* if(foundState){
@@ -112,15 +98,12 @@ function GetState() {
         >
           Click Me!
         </Button>
-        {districts != null
-          ? districts.map((dist, i) => (
-              <ul>
-                <li key={i}>{dist.district_name}</li>
-              </ul>
-            ))
-          : ""}
+        <ul>
+          {districts != null
+            ? districts.map((dist, i) => <li key={i}>{dist.district_name}</li>)
+            : ""}
+        </ul>
       </div>
-      hello
     </>
   );
 }
